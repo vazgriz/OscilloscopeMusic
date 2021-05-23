6fb77c038f96e39348bee58002896e5a34a4af1a
Ryan Vazquez <contact@ryan-vazquez.dev>
2021-05-23 15:31:46 -0500

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <GLFW/glfw3.h>
+
+int main(int argc, const char** argv) {
+    glfwInit();
+
+    if (!glfwVulkanSupported) {
+        std::cerr << "Vulkan is not supported" << std::endl;
+        return 1;
+    }
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Oscilloscope Music", nullptr, nullptr);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
